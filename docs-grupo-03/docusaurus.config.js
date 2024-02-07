fea34b9c9653170a66c5ec6ad4c48cc162d2e101
ipatriciahonorato <patricia.moreira@sou.inteli.edu.br>
2024-02-07 14:09:27 -0300

    Docs: alteração docussaurus

--- docs-grupo-03/docusaurus.config.js
+++ docs-grupo-03/docusaurus.config.js
@@ -46,13 +46,7 @@ const config = {
           editUrl:
           'https://github.com/Inteli-College/2024-T0002-EC09-G03/edit/main/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -78,7 +72,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -118,10 +111,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
