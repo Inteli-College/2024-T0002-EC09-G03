ba275f5eabf0a4249304110f0c2e85242f961bad
Gabi-Barretto <gabriela.dias@sou.inteli.edu.br>
2024-02-22 11:25:55 -0300

    Table no banco - users e alerts
    Consertei um t equivocado postgree no docker-compose

--- src/db/db_init.sql
+++ src/db/db_init.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Sensors (
     id TEXT PRIMARY KEY,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    description TEXT
 );
 
 CREATE TABLE Sensors_Data (
@@ -10,4 +11,20 @@ CREATE TABLE Sensors_Data (
     data TEXT,
     coordinate_x FLOAT(53),
     coordinate_y FLOAT(53)
+);
+
+CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY, -- 'SERIAL' para PostgreSQL
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL, -- Armazena o hash da senha, não a senha em texto claro
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Alerts (
+    alert_id SERIAL PRIMARY KEY,
+    sensor_id TEXT REFERENCES Sensors(id),
+    alert_description TEXT,
+    alert_level VARCHAR(50),
+    alert_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
