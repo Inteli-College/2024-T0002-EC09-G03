2d63a3d4a5f6ff6525cd83d0141009c6091ed485
Lyorrei Shono Quintão <99191948+lyorrei-inteli@users.noreply.github.com>
2024-02-23 16:51:25 -0300

    Removed unecessary code

--- src/sensors_reading/main.go
+++ src/sensors_reading/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"time"
-
 	"github.com/Inteli-College/2024-T0002-EC09-G03/sensors_reading/database"
 	"github.com/Inteli-College/2024-T0002-EC09-G03/sensors_reading/initialization"
 	"github.com/Inteli-College/2024-T0002-EC09-G03/sensors_reading/reader"
@@ -12,5 +10,4 @@ func main() {
 	initialization.LoadEnvVariables()
 	db := database.Connect()
 	reader.Reader(db)
-	database.CreateSensorsData(db, "sensor1", "26.90", 1.0, 1.0, time.Now())
 }
