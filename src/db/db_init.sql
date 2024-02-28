59491a8be9ac42e795179f28708b5917fa8dbe4a
Lyorrei Shono Quintão <99191948+lyorrei-inteli@users.noreply.github.com>
2024-02-28 11:26:27 -0300

    Removidas tabelas Users e Alerts do arquivo db_init.sql

--- src/db/db_init.sql
+++ src/db/db_init.sql
@@ -11,19 +11,3 @@ CREATE TABLE Sensors_Data (
     created_at TIMESTAMP,
     data TEXT
 );
-
-CREATE TABLE Users (
-    user_id SERIAL PRIMARY KEY, -- 'SERIAL' para PostgreSQL
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL, -- Armazena o hash da senha, não a senha em texto claro
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE Alerts (
-    alert_id SERIAL PRIMARY KEY,
-    sensor_id TEXT REFERENCES Sensors(id),
-    alert_description TEXT,
-    alert_level VARCHAR(50),
-    alert_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
